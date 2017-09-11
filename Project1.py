5ffb21bac1158db3a1292d1884650c81fb42d537
Chris <christopher.petrucelli1@marist.edu>
2017-09-11 12:41:40 -0400

    Added transitions between areas, spaces between lines, and began to calculate and print scores

--- Project1.py
+++ Project1.py
@@ -1,8 +1,34 @@
-house = "You arrive home after a long and difficult day of work, like all the others. You are tired, and your room is just down the hall."
+house = "You drop your keys and jacket on the table, exhausted after a long and difficult day of work, like all the others. Your room is just down the hall, waiting for you."
 room = "Your bedroom, with your bed in the center of the back wall. You take your medicine and prepare for bed"
-bed = "Your bed, not as comfortable as it used to be..."
-sleep = "You drift to sleep, and find yourself arriving home after a long and difficult day of work, like all the others."
+bed = "Your bed, not as comfortable as it used to be... Still, you are weary enough to sleep"
+sleep = "You drift to sleep, and find yourself arriving home after a long and difficult day of work, like all the others..."
+intro = "You finally pull into your driveway after your hour drive home. You shut off your car, unlock your front door, and enter."
 playerLocation = house
 playerScore = 0
 title = "Asleep"
 print(title)
+print("======")
+name = input("What is your name? ")
+print()
+print(intro)
+print()
+input("Press 'Enter' to continue")
+playerScore = playerScore + 5
+print()
+print(house)
+print("Your score is", playerScore)
+print()
+input("Press 'Enter' to continue")
+print()
+print(room)
+print()
+input("Press 'Enter' to continue")
+print()
+print(bed)
+print()
+input("Press 'Enter' to continue")
+print()
+print(sleep)
+    
+
+
