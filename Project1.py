8e7bfa9f8af1908bf6ee84c7b5cd5afb52f28b28
Chris <christopher.petrucelli1@marist.edu>
2017-09-11 11:53:31 -0400

    Added locations and some other misc. variables

---
+++ Project1.py
@@ -0,0 +1,8 @@
+house = "You arrive home after a long and difficult day of work, like all the others. You are tired, and your room is just down the hall."
+room = "Your bedroom, with your bed in the center of the back wall. You take your medicine and prepare for bed"
+bed = "Your bed, not as comfortable as it used to be..."
+sleep = "You drift to sleep, and find yourself arriving home after a long and difficult day of work, like all the others."
+playerLocation = house
+playerScore = 0
+title = "Asleep"
+print(title)
