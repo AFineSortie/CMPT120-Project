0f79a979627ce97224b40bfcbbd25ae795597117
Chris <christopher.petrucelli1@marist.edu>
2017-09-20 22:25:04 -0400

    Changed lines to be created using \n. Added header comments. Resolves #4. Changed intro and ending to be controlled with loops. Resolves #3.

--- Project.py
+++ Project.py
@@ -1,70 +1,71 @@
-house = "You drop your keys and jacket on the table, exhausted after a long and difficult day of work, like all the others. Your room is just down the hall, waiting for you."
-room = "Your bedroom, with your bed in the center of the back wall. You take your medicine and prepare for bed."
-bed = "Your bed, not as comfortable as it used to be... Still, you are weary enough to sleep."
-sleep = "You drift to sleep, and find yourself arriving home after a long and difficult day of work, like all the others..."
-intro = "You finally pull into your driveway after your hour drive home. You shut off your car, unlock your front door, and enter."
+# Chris Petrucelli
+# CMPT 120 Project
+# Text-based Adventure
+house = "\nYou drop your keys and jacket on the table, exhausted after a long and difficult day of work, like all the others. Your room is just down the hall, waiting for you.\n"
+room = "\nYour bedroom, with your bed in the center of the back wall. You take your medicine and prepare for bed.\n"
+bed = "\nYour bed, not as comfortable as it used to be... Still, you are weary enough to sleep.\n"
+sleep = "\nYou drift to sleep, and find yourself arriving home after a long and difficult day of work, like all the others...\n"
+intro = "\nYou finally pull into your driveway after your hour drive home. You shut off your car, unlock your front door, and enter.\n"
 playerLocation = house
 playerScore = 0
 title = "Asleep"
+name = "sampleName"
 
-print(title)
-print("======")
-name = input("What is your name? ")
+def start():
+    print(title)
+    print("======\n")
 
-print()
+    global name
+    name = input("What is your name? ")
 
-print(intro)
-print()
+    print(intro)
+
+def end():
+    print("\nTo Be Continued...")
+    print("Final score:", playerScore, "\n")
+    print("(c) 2017 Christopher Petrucelli, christopher.petrucelli1@marist.edu")
+    
+#end() Testing new end function
+
+start()
 
 input("Press 'Enter' to continue")
 playerScore = playerScore + 5
 
-print()
-
 print(house)
-print("Your score is", playerScore)
-
-print()
+print("Your score is", playerScore, "\n")
 
 input("Press 'Enter' to continue")
 playerLocation = room
 playerScore = playerScore + 5
 
-print()
-
 print(room)
-print("Your score is", playerScore)
-
-print()
+print("Your score is", playerScore, "\n")
 
 input("Press 'Enter' to continue")
 playerLocation = bed
 playerScore = playerScore + 5
 
-print()
-
 print(bed)
-print("Your score is", playerScore)
-
-print()
+print("Your score is", playerScore, "\n")
 
 input("Press 'Enter' to continue")
 playerLocation = sleep
 playerScore = playerScore + 5
 
-print()
-
 print(sleep)
-print("Your score is", playerScore)
-
-print()
+print("Your score is", playerScore, "\n")
 
 input("Press 'Enter' to continue")
 
-print()
+end()
 
-print("To Be Continued...")
-print("Final score:", playerScore)
-print("(c) 2017 Christopher Petrucelli, christopher.petrucelli1@marist.edu")
+
+
+
+
+
+
+
 
 
