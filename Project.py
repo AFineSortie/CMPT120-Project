1c43b65a218568686f94b07795a6e1c00ffd089f
Chris <christopher.petrucelli1@marist.edu>
2017-09-27 09:43:52 -0400

    Added new locations and boolean variables for all the locations. Resolves #1 and resolves #2

--- Project.py
+++ Project.py
@@ -5,20 +5,30 @@ house = "\nYou drop your keys and jacket on the table, exhausted after a long an
 room = "\nYour bedroom, with your bed in the center of the back wall. You take your medicine and prepare for bed.\n"
 bed = "\nYour bed, not as comfortable as it used to be... Still, you are weary enough to sleep.\n"
 sleep = "\nYou drift to sleep, and find yourself arriving home after a long and difficult day of work, like all the others...\n"
+houseDream = "\nYou drop your keys and winter jacket on the table, exhausted. It takes you a minute to realize what you're doing, you should be asleep, right? A faint glow is coming from your bedroom down the hall."
 intro = "\nYou finally pull into your driveway after your hour drive home. You shut off your car, unlock your front door, and enter.\n"
 playerLocation = house
 playerScore = 0
 title = "Asleep"
 name = "sampleName"
 
+houseVisit = False
+roomVisit = False
+bedVisit = False
+sleepVisit = False
+houseDreamVisit = False
+roomDreamVisit = False
+
 def start():
     print(title)
     print("======\n")
 
     global name
-    name = input("What is your name? ")
+    name = input("What is your name? ")        
 
     print(intro)
+
+roomDream = "\nYou push open the door, and find the TV still on. A note lies on the bed, with the name", name, "written on the top in her handwriting."
 
 def end():
     print("\nTo Be Continued...")
